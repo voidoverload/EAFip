3935140be9e36cf1b19967a6b79f48cf21411719
voidoverload <voidoverload@gmail.com>
2018-01-26 03:46:21 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+	$time = date("D M j G:i:s T Y");
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$server = var_export($_SERVER, 1);
+	$get = var_export($_GET, 1);
+	$uri = base64_encode($_SERVER['REQUEST_URI']);
+	file_put_contents('../main.txt', $ip."\t".$uri."\t".$time."\n", FILE_APPEND | LOCK_EX);
+?>
---
+++ trace.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PS4='$LINENO: '
+declare -a ip_list
+readarray ip_list < ../ip_list.txt
+((ip_i = ${#ip_list[@]}))
+((count = 0))
+while(("${count}" < "${ip_i}")); do
+	 ip=$(echo "${ip_list[count]}" | cut -f 1)
+	uri=$(echo "${ip_list[count]}" | cut -f 2)
+   req_time=$(echo "${ip_list[count]}" | cut -f 3)
+   mystring=$(geoiplookup -f /usr/share/GeoIP/GeoLiteCity.dat "$ip")
+      nonum=$(echo $mystring | sed 's/[0-9]*//g')
+      mynum=${nonum:26}
+      mynum="${mynum//./}"  #removes periods
+      mynum="${mynum//,/}"  #removes commas
+      mynum="${mynum//-/}"  #removes dashes
+      mynum="${mynum////}"  #removes forward slashes
+      mynum="${mynum//NA/}" #removes NA
+      mynum=$(echo $mynum | xargs)
+	uri=$(echo $uri | base64 --decode)
+finalstring="${req_time} ${ip} ${mynum} ${uri}"
+	echo $finalstring
+	((count++))
+done
