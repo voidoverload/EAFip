fe26b52b5c343f458920209595208ab3e630bb86
voidoverload <voidoverload@gmail.com>
2018-02-06 11:27:42 +0000

    Added checks for apache2, php and geoip-bin

--- setup.sh
+++ setup.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 PS4='$LINENO: '
+    install_php=$(dpkg -s php | grep "Status:" | cut -d " " -f 4)
+  install_geoip=$(dpkg -s geoip-bin | grep "Status:" | cut -d " " -f 4)
+install_apache2=$(dpkg -s apache2 | grep "Status:" | cut -d " " -f 4)
+if [[ "$install_geoip" = "installed"]]; then
+	echo "geoip-bin is installed"
+else
+	apt-get -qq update && apt-get -qq --yes --force-yes install geoip-bin
+	echo "geoip-bin is installed"
+fi
 
-apt-get -qq update && apt-get -qq --yes --force-yes install geoip-bin
-echo "Installing GeoIP"
+if [[ "$install_apache2" = "installed" ]]; then
+	echo "apache2 is installed"
+else
+	apt-get -qq update && apt-get -qq --yes --force-yes install apache2
+	echo "apache2 is installed"
+fi
+
+if [[ "$install_php" = "installed"]]; then
+	echo "php is installed"
+else
+	apt-get --qq update && apt-get -qq --yes --force-yes install php
+	echo "php is installed"
+fi
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz -O /usr/share/GeoIP/GeoLiteCity.dat.gz
 gzip -d /usr/share/GeoIP/GeoLiteCity.dat.gz
 echo "Moving files to Document Root"
