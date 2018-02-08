ad5cd516c84236d9c66585ac5402006bd810e20a
voidoverload <voidoverload@gmail.com>
2018-02-08 11:30:11 +0000

    fix if statements for determining installed packages

--- setup.sh
+++ setup.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
 PS4='$LINENO: '
-    install_php=$(dpkg -s php | grep "Status:" | cut -d " " -f 4)
-  install_geoip=$(dpkg -s geoip-bin | grep "Status:" | cut -d " " -f 4)
-install_apache2=$(dpkg -s apache2 | grep "Status:" | cut -d " " -f 4)
-if [[ "$install_geoip" = "installed"]]; then
-	echo "geoip-bin is installed"
+
+if ! [ -x "$(command -v php)"  ]; then
+	apt-get -qq update && apt-get -qq --yes --force-yes install php
 else
-	apt-get -qq update && apt-get -qq --yes --force-yes install geoip-bin
-	echo "geoip-bin is installed"
+	echo "PHP is installed"
 fi
 
-if [[ "$install_apache2" = "installed" ]]; then
-	echo "apache2 is installed"
-else
+if ! [ -x "$(command -v apache2)" ]; then
 	apt-get -qq update && apt-get -qq --yes --force-yes install apache2
+else
 	echo "apache2 is installed"
 fi
 
-if [[ "$install_php" = "installed"]]; then
-	echo "php is installed"
+if ! [ -x "$(command -v geoip-bin)" ]; then
+	apt-get -qq update && apt-get -qq --yes --force-yes install geoip-bin
 else
-	apt-get --qq update && apt-get -qq --yes --force-yes install php
-	echo "php is installed"
+	echo "geoip-bin is installed"
 fi
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz -O /usr/share/GeoIP/GeoLiteCity.dat.gz
-gzip -d /usr/share/GeoIP/GeoLiteCity.dat.gz
+
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz -O GeoLiteCity.dat.gz
+gzip -d GeoLiteCity.dat.gz
+mv GeoLiteCity.dat /usr/share/GeoIP/GeoLiteCity.dat
 echo "Moving files to Document Root"
 find_d=$(grep -n -e "DocumentRoot" /etc/apache2/sites-available/default-ssl.conf)
 d_root=$(echo $find_d | cut -d " " -f 3)
-mv trace.sh $rootfolder/trace.sh
-mv index.php $rootfolder/index.php
+mv trace.sh "$rootfolder/trace.sh"
+mv index.php "$rootfolder/index.php"
 echo "Finished!"
