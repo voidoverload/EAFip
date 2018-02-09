761902e5c27c664d0cbb48fbebe0ea88a0cd2291
voidoverload <voidoverload@gmail.com>
2018-02-09 05:32:52 +0000

    added chmod and moved apt-get update

--- setup.sh
+++ setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 PS4='$LINENO: '
-
+apt-get -qq update
 if ! [ -x "$(command -v php)"  ]; then
-        apt-get -qq update && apt-get -qq --yes --force-yes install php
+        apt-get -qq --yes --force-yes install php
 else
         echo "PHP is installed"
 fi
 
 if ! [ -x "$(command -v apache2)" ]; then
-        apt-get -qq update && apt-get -qq --yes --force-yes install apache2
+        apt-get -qq --yes --force-yes install apache2
 else
         echo "apache2 is installed"
 fi
 
 if ! [ -x "$(command -v geoip-bin)" ]; then
-        apt-get -qq update && apt-get -qq --yes --force-yes install geoip-bin
+        apt-get -qq --yes --force-yes install geoip-bin
 else
         echo "geoip-bin is installed"
 fi
@@ -27,4 +27,6 @@ find_d=$(grep -n -e "DocumentRoot" /etc/apache2/sites-available/default-ssl.conf
 d_root=$(echo $find_d | cut -d " " -f 3)
 mv trace.sh "$d_root"/trace.sh
 mv index.php "$d_root"/index.php
+touch "$d_root"/../ip_list.txt
+chmod 777 "$d_root"/../ip_list.txt
 echo "Finished!"
